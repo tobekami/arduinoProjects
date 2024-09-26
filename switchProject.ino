26325fbd5479515422273f79939634239ee26237
tobekami <brunoemelife@gmail.com>
2024-09-26 11:30:03 +0100

    Added comments

--- switchProject.ino
+++ switchProject.ino
@@ -1,16 +1,19 @@
-#define SWITCH_PIN 2
+#define SWITCH_PIN 2 // Define the pin number for the switch
 
 void setup() {
-  pinMode(SWITCH_PIN, INPUT_PULLUP); // Enable internal pull-up resistor
-  Serial.begin(9600);
+  pinMode(SWITCH_PIN, INPUT_PULLUP); // Set the switch pin as input with an internal pull-up resistor
+  Serial.begin(9600); // Start serial communication at a baud rate of 9600 for debugging
 }
 
 void loop() {
-  int switchState = digitalRead(SWITCH_PIN);
+  int switchState = digitalRead(SWITCH_PIN); // Read the state of the switch (HIGH or LOW)
+  
+  // Check if the switch is pressed (LOW because of the pull-up resistor)
   if (switchState == LOW) {
-    Serial.println("Switch Pressed");
+    Serial.println("Switch Pressed"); // Print message to the Serial Monitor when pressed
   } else {
-    Serial.println("Switch Released");
+    Serial.println("Switch Released"); // Print message when the switch is not pressed
   }
-  delay(500);
+  
+  delay(500); // Add a delay of 500ms between readings to prevent spamming the Serial Monitor
 }
